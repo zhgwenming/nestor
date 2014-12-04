838aee63b7d298ccbe985fb41b77db7dff10b2be
Albert Zhang <zhgwenming@gmail.com>
2014-12-04 14:57:45 +0800

    pid check wip

--- exec.go
+++ exec.go
@@ -82,3 +82,47 @@ func (c *Cmd) TermRun() error {
 
 	return c.Cmd.Wait()
 }
+
+func CheckProcessFromPid(pidfile string) bool {
+	var file *os.File
+
+	if _, err := os.Stat(pidfile); os.IsNotExist(err) {
+		if file, err = os.Create(pidfile); err != nil {
+			return err
+		}
+	} else {
+		if file, err = os.OpenFile(pidfile, os.O_RDWR, 0); err != nil {
+			return err
+		}
+		pidstr := make([]byte, 8)
+
+		n, err := file.Read(pidstr)
+		if err != nil {
+			return err
+		}
+
+		if n > 0 {
+			pid, err := strconv.Atoi(string(pidstr[:n]))
+			if err != nil {
+				fmt.Printf("err: %s, overwriting pidfile", err)
+			}
+
+			process, _ := os.FindProcess(pid)
+			if err = process.Signal(syscall.Signal(0)); err == nil {
+				return fmt.Errorf("pid: %d is running", pid)
+			} else {
+				fmt.Printf("err: %s, cleanup pidfile", err)
+			}
+
+			if file, err = os.Create(pidfile); err != nil {
+				return err
+			}
+
+		}
+
+	}
+
+	pid := strconv.Itoa(os.Getpid())
+	fmt.Fprintf(file, "%s", pid)
+	return nil
+}
